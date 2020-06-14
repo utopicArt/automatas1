f45fbe2c8b2695cc9a2102608df3a8a79e7ae67c
Adrian-Marin <pilotoadrian@hotmail.com>
2020-06-14 00:46:19 -0500

    Lectura web webay y ebay
    Se leen las tres palabras indicadas

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     
     var wordChar;
     var contadorDosPalabras=0;
-    var dosPalabras;
+    var palabra;
     
     var palabraTemp = "";
     var inLocatedFlag=false;
@@ -14,27 +14,16 @@ $(document).ready(function(){
         "o","p","q","r","s","t","u",
         "v","v-2","w","x","y","z","aa","ab",
     ];
+    var finPalabra=false;
     function readWordChar(word, cnt){
         setTimeout(function(){
             $(".word-container .word").append(word);
             //$("svg g#"+arrowID[cnt]+" .cls-1").css('stroke','6DC009');
-            if(contadorDosPalabras==2){
-                palabraTemp=word;
-                if(inLocatedFlag==true){
-                    checkWeb(dosPalabrasTemp);
-                }else{
-                    checkWeb(dosPalabras);
-                }                
-                dosPalabrasTemp = dosPalabras;
-                //alert("Dos palabras -"+dosPalabras+"-");
-                dosPalabras = "";
-                contadorDosPalabras = 0;
-            }
             if(word==' '||word==undefined||word=='\n'){
-                dosPalabras = "";
+                checkWeb(palabra);
                 contadorDosPalabras = 0;
             }else{
-                dosPalabras += word;
+                palabra += word;
                 contadorDosPalabras++;
             }
         },10*cnt);
@@ -47,33 +36,51 @@ $(document).ready(function(){
         } 
     });
     
+    
+    //$(".word-web .word").append("Web,");
+    //$(".word-web .counter span").text(encontradas);
+    //console.log(cn+".- Se encontró Web!");
+    
     var cn = 1;
     var webEncontradas = 0;
+    var webayEncontradas=0;
+    var ebayEncontradas=0;
     var completa;
     function checkWeb(caracter){
         caracter = caracter.toLocaleLowerCase();
-        if(caracter==="we"){
-            inLocatedFlag=true;
-            console.log(cn+".- Se encontró We");
-            if(palabraTemp=='b'){
-                completa+='eb';
-                webEncontradas++;
-                $(".word-web .word").append("Web,");
-                $(".word-web .counter span").text(webEncontradas);
-               console.log(cn+".- Se encontró Web!");
-            }else{
-                completa="";
-                console.log(cn+".- No era Web :c -"+palabraTemp+"-");
-                palabraTemp="";
-                inLocatedFlag=false;
+        console.log(cn+".- "+caracter);
+        for(var i=0;i<caracter.length;i++){
+            cn = i+1;
+            if(caracter.substring(i,i+1)=='w'){
+                if(caracter.substring(i+1,i+2)=='e'){
+                    if(caracter.substring(i+2,i+3)=='b'){
+                        webEncontradas++;
+                        $(".word-web .word").append(webEncontradas+".- "+caracter+"\n\n");
+                        $(".word-web .counter span").text(webEncontradas);
+                        console.log(cn+".- Se encontró Web!");
+                        if(caracter.substring(i+3,i+4)=='a'){
+                            if(caracter.substring(i+4,i+5)=='y'){
+                                webayEncontradas++;
+                                $(".word-webay .word").append(webayEncontradas+".- "+caracter+"\n\n");
+                                $(".word-webay .counter span").text(webayEncontradas);
+                                console.log(cn+".- Se encontró Webay!");
+                            }
+                        }                      
+                    }
+                }
+            }else if(caracter.substring(i,i+1)=='e'){
+                if(caracter.substring(i+1,i+2)=='b'){
+                   if(caracter.substring(i+2,i+3)=='a'){
+                       if(caracter.substring(i+3,i+4)=='y'){
+                            ebayEncontradas++;
+                            $(".word-ebay .word").append(ebayEncontradas+".- "+caracter+"\n\n");
+                            $(".word-ebay .counter span").text(ebayEncontradas);
+                            console.log(cn+".- Se encontró Webay!");                           
+                       }
+                   } 
+                }
             }
-            cn++
         }
-        if(completa==="eb"){
-            inLocatedFlag=true;
-            console.log(cn+".- Se encontró Eb");      
-        }else{
-            console.log("No coincide");
-        }
+        palabra = "";
     }
 });
